fe354386280916e9e78f43b56505cb731843a38a
cclen <cece.len12@gmail.com>
2018-08-06 19:51:59 -0700

    creating first round buttons in library

--- assets/buttons/buttons.js
+++ assets/buttons/buttons.js
@@ -1,1 +1,19 @@
-var button = {};
+// var buttons = document.getElementsByClassName('button');
+// console.log('buttons: ', buttons);
+
+var fn = function () {
+
+  var minus = document.getElementsByClassName('minus');
+  console.log('minus: ', minus);
+
+  for(var i = 0; i < minus.length; i++){
+    var e = document.createElement('p');
+    e.innerHTML = '-';
+    minus[i].appendChild(e);
+    console.log('hello');
+    // console.log('minus[i]: ' , minus[i]);
+  }
+
+}
+
+document.addEventListener('DOMContentLoaded', fn, false);
--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,6 +1,23 @@
-"use strict";
+'use strict';
 
-var button = {};
+// var buttons = document.getElementsByClassName('button');
+// console.log('buttons: ', buttons);
+
+var fn = function fn() {
+
+  var minus = document.getElementsByClassName('minus');
+  console.log('minus: ', minus);
+
+  for (var i = 0; i < minus.length; i++) {
+    var e = document.createElement('p');
+    e.innerHTML = '-';
+    minus[i].appendChild(e);
+    console.log('hello');
+    // console.log('minus[i]: ' , minus[i]);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', fn, false);
 'use strict';
 
 var list = [];
