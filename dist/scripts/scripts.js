b9dcf84c7b73447b4b5e2240edef7e53fcb76cf3
cclen <cece.len12@gmail.com>
2018-08-12 09:14:31 -0700

    adding buttons style and functionality

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,23 +1,50 @@
 'use strict';
 
-// var buttons = document.getElementsByClassName('button');
-// console.log('buttons: ', buttons);
-
 var fn = function fn() {
 
-  var minus = document.getElementsByClassName('minus');
-  console.log('minus: ', minus);
+  function addText(className, text) {
 
-  for (var i = 0; i < minus.length; i++) {
-    var e = document.createElement('p');
-    e.innerHTML = '-';
-    minus[i].appendChild(e);
-    console.log('hello');
-    // console.log('minus[i]: ' , minus[i]);
+    var arr = document.getElementsByClassName(className);
+
+    for (var i = 0; i < arr.length; i++) {
+      var e = document.createElement('p');
+      e.innerHTML = text;
+      arr[i].appendChild(e);
+    }
   }
+
+  addText('minus', '-');
+  addText('plus', '+');
+  addText('inactive', 'inactive');
+  addText('normal', 'normal');
+  addText('hover-web', 'hover[web only]');
+  addText('focused-click', 'click/tap');
+  addText('check', '<i class="fas fa-check"></i>');
 };
 
 document.addEventListener('DOMContentLoaded', fn, false);
+
+//   var buttons =
+//     { minus : "-",
+//       plus: "+",
+//       inactive: "inactive" };
+//
+//
+// var renderTextToButtons = function(arr){
+//   let keys = Object.keys(arr);
+//
+//   let values = (function(arr){
+//     var result =[];
+//     for(let key in arr) {
+//       result.push(arr[key]);
+//     }
+//   return result;
+// })();
+// console.log(values);
+//
+// };
+//
+// renderTextToButtons(buttons);
 'use strict';
 
 var list = [];
